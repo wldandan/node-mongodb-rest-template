e3433df43bac64fc9fe374567148264e6a6b15d3
Wang Lei <leiwang@thoughtworks.com>
2013-12-18 00:02:57 +0800

    Lei: init the nodejs+mongodb=>flexible rest service

---
+++ app.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+routes  = require( './lib/routes' );
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server  = http.createServer( app );
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+routes.configRoutes( app, server );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
